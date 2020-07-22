90e1e521d27263096d6c30e64c766d85fc06eae1
Declan Valters <decval@bgs.ac.uk>
2020-07-22 16:42:53 +0100

    Start refactor split tests for test_oracle_setup

--- test/unit/test_setup_oracle_client.py
+++ test/unit/test_setup_oracle_client.py
@@ -14,10 +14,7 @@ from etlhelper.setup_oracle_client import (
 
 @pytest.mark.parametrize("emsg,expected_return,expected_text",
                          [("ORA-12162: some error", True, ""),
-                          ("DPI-1047: libnsl.so.1", False, NSL_MESSAGE + "\n"),
-                          ("DPI-1047: libclntsh.so", False, CLNTSH_MESSAGE + "\n"),
-                          ("DPI-9999: Some weird error", False,
-                              "cx_Oracle Instant Client Error: DPI-9999: Some weird error\n")])
+                          ("DPI-1047: libclntsh.so", False, CLNTSH_MESSAGE + "\n")])
 def test_oracle_client_is_configured(monkeypatch, capfd, emsg, expected_return, expected_text):
     """
     Tests the oracle_client_is_configured returns False given cetain libs
@@ -43,3 +40,25 @@ def test_oracle_client_is_configured(monkeypatch, capfd, emsg, expected_return, 
     # Assert
     assert result is expected_return
     assert out == expected_text
+
+@pytest.mark.parametrize("emsg, expected_text",
+                         [("DPI-1047: libnsl.so.1", NSL_MESSAGE + "\n"),
+                          ("DPI-9999: Some weird error",
+                              "cx_Oracle Instant Client Error: DPI-9999: Some weird error\n")])
+def test_oracle_client_is_configured_sys_exits(monkeypatch, emsg, expected_text):
+    # Arrange cx_Oracle to raise error when connect() is called
+    cx_err = MagicMock(_Error)
+    cx_err.message = emsg
+
+    def mock_connect(dummy):
+        raise DatabaseError(cx_err)
+    monkeypatch.setattr(cx_Oracle, 'connect', mock_connect)
+
+    # Act
+    with pytest.raises(SystemExit):
+        _oracle_client_is_configured()
+
+    # out, err = capfd.readouterr()
+    # Assert
+    # assert result is expected_return
+    # assert out == expected_text
