3b2c717e377e134c03eb44f6f4e2f750c6f6da0b
Colin Blackburn <colb@bgs.ac.uk>
2020-08-19 14:09:50 +0100

    Add integration tests for bad param style

--- test/integration/db/test_oracle.py
+++ test/integration/db/test_oracle.py
@@ -10,7 +10,8 @@ import cx_Oracle
 import pytest
 
 from etlhelper import connect, get_rows, copy_rows, DbParams
-from etlhelper.exceptions import ETLHelperConnectionError
+from etlhelper.exceptions import ETLHelperConnectionError, ETLHelperInsertError
+
 from test.conftest import db_is_unreachable
 
 # Skip these tests if database is unreachable
@@ -51,6 +52,14 @@ def test_copy_rows_happy_path(test_tables, testdb_conn, test_table_data):
         assert result[i] == tuple(row_with_datetimes)
 
 
+def test_copy_rows_bad_param_style(test_tables, testdb_conn, test_table_data):
+    # Arrange and act
+    select_sql = "SELECT * FROM src"
+    insert_sql = BAD_PARAM_STYLE_SQL.format(tablename='dest')
+    with pytest.raises(ETLHelperInsertError):
+        copy_rows(select_sql, testdb_conn, insert_sql, testdb_conn)
+
+
 # -- Fixtures here --
 
 INSERT_SQL = dedent("""
@@ -58,6 +67,14 @@ INSERT_SQL = dedent("""
       day, date_time)
     VALUES
       (:1, :2, :3, :4, :5, :6)
+      """).strip()
+
+
+BAD_PARAM_STYLE_SQL = dedent("""
+    INSERT INTO {tablename} (id, value, simple_text, utf8_text,
+      day, date_time)
+    VALUES
+      (?, ?, ?, ?, ?, ?)
       """).strip()
 
 
--- test/integration/db/test_sqlite.py
+++ test/integration/db/test_sqlite.py
@@ -11,7 +11,11 @@ from textwrap import dedent
 import pytest
 
 from etlhelper import connect, get_rows, copy_rows, execute, DbParams
-from etlhelper.exceptions import ETLHelperConnectionError, ETLHelperQueryError
+from etlhelper.exceptions import (
+    ETLHelperConnectionError,
+    ETLHelperQueryError,
+    ETLHelperInsertError
+)
 
 
 # -- Tests here --
@@ -75,6 +79,14 @@ def test_copy_rows_happy_path(test_tables, testdb_conn, test_table_data):
     assert fixed == test_table_data
 
 
+def test_copy_rows_bad_param_style(test_tables, testdb_conn, test_table_data):
+    # Arrange and act
+    select_sql = "SELECT * FROM src"
+    insert_sql = BAD_PARAM_STYLE_SQL.format(tablename='dest')
+    with pytest.raises(ETLHelperInsertError):
+        copy_rows(select_sql, testdb_conn, insert_sql, testdb_conn)
+
+
 def test_get_rows_with_parameters(pgtestdb_test_tables, pgtestdb_conn,
                                   test_table_data):
     # parameters=None is tested by default in other tests
@@ -92,6 +104,14 @@ INSERT_SQL = dedent("""
       day, date_time)
     VALUES
       (?, ?, ?, ?, ?, ?)
+      """).strip()
+
+
+BAD_PARAM_STYLE_SQL = dedent("""
+    INSERT INTO {tablename} (id, value, simple_text, utf8_text,
+      day, date_time)
+    VALUES
+      (%1, %2, %3, %4, %5, %6)
       """).strip()
 
 
