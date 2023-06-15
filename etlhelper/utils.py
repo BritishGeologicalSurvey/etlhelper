6b7d8f947b764fd4b0ef5ef3dfb5b86c3432b596
Leo Rudczenko <leorud@bgs.ac.uk>
2023-06-15 16:22:28 +0100

    Add return and raises values to docstrings

--- etlhelper/utils.py
+++ etlhelper/utils.py
@@ -17,7 +17,9 @@ def table_info(table, conn, schema=None):
     :param table: str, the table to describe
     :param conn: dbapi connection
     :param schema: str, optional name of schema for table
-    :returns columns: list, tuples of (name, type, not_null, has_default)
+    :returns: a list of columns, where each column is a tuple of 
+              (name, type, not_null, has_default)
+    :raises ETLHelperQueryError: if a table is not unique in the database
     """
     helper = DB_HELPER_FACTORY.from_conn(conn)
 
